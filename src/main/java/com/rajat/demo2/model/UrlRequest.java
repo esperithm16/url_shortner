2ac923e074b08e7b043d785315c0a1a74e514272
esperithm16 <rajatshaily17@gmail.com>
2026-02-26 23:00:48 +0530

    day 2

--- src/main/java/com/rajat/demo2/model/UrlRequest.java
+++ src/main/java/com/rajat/demo2/model/UrlRequest.java
@@ -1,14 +1,11 @@
 package com.rajat.demo2.model;
 
-public class UrlRequest {
-
-    private String url;
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
+public class UrlRequest{
+private String url;
+public String getUrl(){
+    return  url;
+}
+public void setUrl(String url){
+    this.url()=url;
+}
 }
---
+++ src/main/java/com/rajat/demo2/service/urlService.java
@@ -0,0 +1,12 @@
+package com.rajat.demo2.service;
+
+import org.springframework.stereotype.Service;
+
+public class urlService {
+@Service
+    public class urlService{
+    public String generateShortUrl(String longUrl){
+      return "short123";
+    }
+}
+}
