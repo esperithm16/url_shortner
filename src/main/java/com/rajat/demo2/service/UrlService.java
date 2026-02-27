1fc018bf18cd0173547aac1f062e3754779884a8
esperithm16 <rajatshaily17@gmail.com>
2026-02-27 22:37:22 +0530

    day 2

--- src/main/java/com/rajat/demo2/service/UrlService.java
+++ src/main/java/com/rajat/demo2/service/UrlService.java
@@ -2,8 +2,11 @@ package com.rajat.demo2.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+
 @Service
 public class UrlService {
+    private Map<String ,String>urlMap=new HashMap<>();
 
     public String generateShortUrl(String longUrl) {
         return "short123";
