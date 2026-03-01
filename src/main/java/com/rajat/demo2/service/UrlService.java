928da4ffc20143a17cfdcaf44f3971e70bd327fe
esperithm16 <rajatshaily17@gmail.com>
2026-03-01 22:52:34 +0530

    day 5

--- src/main/java/com/rajat/demo2/service/UrlService.java
+++ src/main/java/com/rajat/demo2/service/UrlService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @Service
 public class UrlService {
@@ -16,5 +17,14 @@ public class UrlService {
     }
     public String getOriginalUrl(String shortCode){
        return urlMap.get(shortCode);
+    }
+    private String generateRandomCode(){
+        String characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder shortCode=new StringBuilder();
+        Random random=new Random();
+        for (int i = 0 ; i<6;i++){
+            shortCode.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return shortCode.toString();
     }
 }
