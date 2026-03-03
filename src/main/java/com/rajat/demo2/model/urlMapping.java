9cd3a44bdf2a9f608f0a9b78c70388c1657dd05c
esperithm16 <rajatshaily17@gmail.com>
2026-03-03 19:01:35 +0530

    day6and7

---
+++ src/main/java/com/rajat/demo2/model/urlMapping.java
@@ -0,0 +1,38 @@
+package com.rajat.demo2.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UrlMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String shortCode;
+
+    private String originalUrl;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+}
