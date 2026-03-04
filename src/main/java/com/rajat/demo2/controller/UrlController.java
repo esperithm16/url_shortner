eeafa6077037c01174d653687fb266bff50c8220
esperithm16 <rajatshaily17@gmail.com>
2026-03-04 22:08:42 +0530

    day8

--- src/main/java/com/rajat/demo2/controller/UrlController.java
+++ src/main/java/com/rajat/demo2/controller/UrlController.java
@@ -1,4 +1,23 @@
 package com.rajat.demo2.controller;
 
+import com.rajat.demo2.dto.UrlRequest;
+import com.rajat.demo2.dto.ShortenResponse;
+import com.rajat.demo2.service.UrlService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class UrlController {
+
+    private final UrlService urlService;
+
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+
+    @PostMapping("/shorten")
+    public ShortenResponse shortenUrl(@Valid @RequestBody UrlRequest request) {
+
+        return urlService.shortenUrl(request.getUrl());
+    }
 }
