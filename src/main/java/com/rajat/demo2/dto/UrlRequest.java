a9f40986d004963eacea5e2174b6aec513f2c2fa
esperithm16 <rajatshaily17@gmail.com>
2026-03-04 21:55:09 +0530

    day8

--- src/main/java/com/rajat/demo2/dto/UrlRequest.java
+++ src/main/java/com/rajat/demo2/dto/UrlRequest.java
@@ -1,4 +1,17 @@
-package com.rajat.demo2.dto;
+package com.example.demo2.dto;
+
+import jakarta.validation.constraints.NotBlank;
 
 public class UrlRequest {
+
+    @NotBlank
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
