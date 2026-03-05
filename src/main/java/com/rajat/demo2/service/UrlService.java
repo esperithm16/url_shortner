56626bb58718b775245d03c2daad9623bacb819f
esperithm16 <rajatshaily17@gmail.com>
2026-03-05 22:45:53 +0530

    day8

--- src/main/java/com/rajat/demo2/service/UrlService.java
+++ src/main/java/com/rajat/demo2/service/UrlService.java
@@ -1,5 +1,6 @@
 package com.rajat.demo2.service;
 
+import com.rajat.demo2.dto.ShortenResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -8,29 +9,41 @@ import java.util.Random;
 
 @Service
 public class UrlService {
-    private Map<String ,String> urlMap=new HashMap<>();
 
-    private String generateRandomCode(){
-        String characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        StringBuilder shortCode=new StringBuilder();
-        Random random=new Random();
-        for (int i = 0 ; i<6;i++){
-            shortCode.append(characters.charAt(random.nextInt(characters.length())));
+    private Map<String, String> urlMap = new HashMap<>();
+
+    public ShortenResponse shortenUrl(String longUrl) {
+
+        String shortCode;
+
+        do {
+            shortCode = generateRandomCode();
+        } while (urlMap.containsKey(shortCode));
+
+        urlMap.put(shortCode, longUrl);
+
+        return new ShortenResponse(shortCode);
+    }
+
+    public String getOriginalUrl(String shortCode) {
+        return urlMap.get(shortCode);
+    }
+
+    private String generateRandomCode() {
+
+        String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        StringBuilder shortCode = new StringBuilder();
+
+        Random random = new Random();
+
+        for (int i = 0; i < 6; i++) {
+
+            int index = random.nextInt(characters.length());
+
+            shortCode.append(characters.charAt(index));
         }
+
         return shortCode.toString();
     }
-    public String generateShortUrl(String longUrl) {
-       String shortCode;
-
-       do{
-           shortCode=generateRandomCode();
-       }while (urlMap.containsKey(shortCode));
-
-        urlMap.put(shortCode,longUrl);
-       return shortCode;
-    }
-    public String getOriginalUrl(String shortCode){
-       return urlMap.get(shortCode);
-    }
-
 }
