3338134754acb7e227edd47c318652ff86ca4bda
esperithm16 <rajatshaily17@gmail.com>
2026-03-05 22:30:09 +0530

    day8

--- src/main/java/com/rajat/demo2/controller/UrlController.java
+++ src/main/java/com/rajat/demo2/controller/UrlController.java
@@ -4,7 +4,11 @@ import com.rajat.demo2.dto.UrlRequest;
 import com.rajat.demo2.dto.ShortenResponse;
 import com.rajat.demo2.service.UrlService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 public class UrlController {
@@ -19,5 +23,15 @@ public class UrlController {
     public ShortenResponse shortenUrl(@Valid @RequestBody UrlRequest request) {
 
         return urlService.shortenUrl(request.getUrl());
+    }
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortCode) {
+
+        String originalUrl = urlService.getOriginalUrl(shortCode);
+
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .location(URI.create(originalUrl))
+                .build();
     }
 }
