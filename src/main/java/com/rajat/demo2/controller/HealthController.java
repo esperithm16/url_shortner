91c0b242ce0d2138283164a0f589cdb29634e0bf
esperithm16 <rajatshaily17@gmail.com>
2026-03-07 23:45:43 +0530

    10

--- src/main/java/com/rajat/demo2/controller/HealthController.java
+++ src/main/java/com/rajat/demo2/controller/HealthController.java
@@ -1,43 +1,12 @@
-package com.rajat.demo2.controller;
-
-import com.rajat.demo2.service.UrlService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
-    private final UrlService urlService;
-
-    public HealthController(UrlService urlService) {
-        this.urlService = urlService;
-    }
-
-    // Day 1 API
     @GetMapping("/test")
-    public String test() {
+    public String test(){
         return "Spring Boot Running 🚀";
     }
 
-    // Day 2 API
-
-    @GetMapping("/{shortCode}")
-    public ResponseEntity<Void> redirect(@PathVariable String shortCode) {
-
-        String originalUrl = urlService.getOriginalUrl(shortCode);
-
-        if (originalUrl == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity
-                .status(HttpStatus.FOUND)
-                .location(URI.create(originalUrl))
-                .build();
-    }
 }
