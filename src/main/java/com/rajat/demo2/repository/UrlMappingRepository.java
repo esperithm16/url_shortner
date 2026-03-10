acda25688682e3e21730a7ee29d66ef420d4476c
esperithm16 <rajatshaily17@gmail.com>
2026-03-10 22:24:12 +0530

    add UrlMappingRepository

---
+++ src/main/java/com/rajat/demo2/repository/UrlMappingRepository.java
@@ -0,0 +1,12 @@
+package com.rajat.demo2.Repository;
+
+import com.rajat.demo2.model.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+
+    Optional<UrlMapping> findByShortCode(String shortCode);
+
+}
