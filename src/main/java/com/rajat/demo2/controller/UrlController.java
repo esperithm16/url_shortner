ca7cb7009bc3450b512be511aa35b09218a3d8a7
esperithm16 <rajatshaily17@gmail.com>
2026-03-14 09:41:18 +0530

    final

--- src/main/java/com/rajat/demo2/controller/UrlController.java
+++ src/main/java/com/rajat/demo2/controller/UrlController.java
@@ -1,4 +1,5 @@
 package com.rajat.demo2.controller;
+import com.rajat.demo2.dto.UrlRequest;
 import com.rajat.demo2.service.UrlService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.*;
@@ -23,9 +24,7 @@ public class UrlController {
         response.sendRedirect(originalUrl);
     }
     @PostMapping("/shorten")
-    public String shortenUrl(@RequestBody String url){
-
-        return urlService.createShortUrl(url);
-
+    public String shortenUrl(@RequestBody UrlRequest request){
+        return urlService.createShortUrl(request.getUrl());
     }
 }
